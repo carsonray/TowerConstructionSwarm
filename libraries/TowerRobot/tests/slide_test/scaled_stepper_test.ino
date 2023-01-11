b32ff37ea63555e5c1f94400c97100a35da270e2
Carson Ray <carsonray314@gmail.com>
2023-01-11 08:52:54 -0500

    added scaled stepper test

---
+++ libraries/TowerRobot/tests/slide_test/scaled_stepper_test.ino
@@ -0,0 +1,28 @@
+// Include the ScaledStepper Library
+#include <ScaledStepper.h>
+
+// Define parameters
+const int stepPin = 12;
+const int dirPin = 13;
+const int modePins[3] = {9, 10, 11};
+
+// Creates a scaled stepper instance
+ScaledStepper stepper(stepPin, dirPin, modePins[0], modePins[1], modePins[2]);
+
+void setup() {
+  // set the maximum speed, acceleration factor,
+  // initial speed and the target position
+  myStepper.setMaxSpeed(1000);
+  myStepper.setAcceleration(50);
+  myStepper.setSpeed(200);
+  myStepper.moveTo(STEPS_PER_REV);
+}
+
+void loop() {
+  // Change direction once the motor reaches target position
+  if (myStepper.distanceToGo() == 0) 
+    myStepper.moveTo(-myStepper.currentPosition());
+
+  // Move the motor one step
+  myStepper.run();
+}
